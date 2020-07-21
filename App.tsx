dfda42a2c1518a2343a5026638d54904e3de1614
Albinovych <dmitryalbinovych@gmail.com>
2020-07-21 09:59:19 +0300

    add react-native-svg packege, create Header element

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import Counter from './src/components/Counter';
 import DrinkList from './src/container/DrinkList';
+import Header from './src/components/Header';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+        <Header />
         <DrinkList header="Dummy header" />
       </SafeAreaView>
     </>
